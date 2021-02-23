9232c3d956a5611e826dd25f773eb3b52f60636d
AntoniPizarro <apizarro@cifpfbmoll.eu>
2021-02-23 15:40:35 +0100

    feat: Backstage y Sulfuras
    Se ha implementado la logica de sulfuras y backstage

--- Java/guildedrose/src/main/java/edu/pingpong/guildedrose/updatable/Backstage.java
+++ Java/guildedrose/src/main/java/edu/pingpong/guildedrose/updatable/Backstage.java
@@ -1,1 +1,24 @@
 package edu.pingpong.guildedrose.updatable;
+
+public class Backstage extends NormalItem {
+
+    public Backstage(String name, int sellIn, int quality) {
+        super(name, sellIn, quality);
+    }
+
+    @Override
+    public void updateQuality() {
+        setSellIn(-1);
+        computeQuality(1);
+        if (getSellIn() < 11) {
+            computeQuality(1);
+        }
+        if (getSellIn() < 6) {
+            computeQuality(1);
+        }
+        if (getSellIn() <= 0) {
+            computeQuality(-getQuality());
+        }
+    }
+
+}
--- Java/guildedrose/src/main/java/edu/pingpong/guildedrose/updatable/Sulfuras.java
+++ Java/guildedrose/src/main/java/edu/pingpong/guildedrose/updatable/Sulfuras.java
@@ -1,1 +1,19 @@
 package edu.pingpong.guildedrose.updatable;
+
+public class Sulfuras extends NormalItem{
+
+    public Sulfuras(String name, int sellIn, int quality) {
+        super(name, sellIn, quality);
+    }
+
+    @Override
+    public void updateQuality() {
+        while (getQuality() < 80) {
+            computeQuality(1);
+        }
+        while (getQuality() > 80) {
+            computeQuality(-1);
+        }
+    }
+
+}
